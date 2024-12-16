f265855649a4e3b830f81e664a9af63c56ba9800
Dima Tim <qa1717718@gmail.com>
2024-12-16 19:55:06 +0300

    Лекция 12.

--- src/main/java/org/example/Figura.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public interface Figura {
-    public void Perimetr();
-    public void Area();
-    public void showInfo();
-}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,21 @@
 package org.example;
 
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //  Задание 1
-        System.out.println("Задание 1:");
-        Animal animal = new Animal();
-        animal.Feed(50);
-        Dog dog = new Dog("Бобик");
-        dog.Run(19);
-        Cat cat = new Cat("Гарфилд", 10);
-        cat.Swimm(20);
-        Cat[] cats = new Cat[3];
-        cats[0] = new Cat("Гоча", 10);
-        cats[1] = new Cat("Сигма", 30);
-        cats[2] = new Cat("Маня", 20);
-        // Звдвние 2
-        System.out.println("Задание 2:");
-        Circle circle = new Circle("Крансый", "Зеленый", 12);
-        Square square = new Square("Синий", "Беылй",5);
-        Triangle triangle = new Triangle("Фиолетовый", "Серый", 4,3,2);
-        circle.Perimetr();
-        circle.Area();
-        square.Perimetr();
-        square.Area();
-        triangle.Perimetr();
-        triangle.Area();
-        circle.showInfo();
-        square.showInfo();
-        triangle.showInfo();
-    }
+        String[][] str = new String[5][5];
 
+        try{
+            MyArray(str);
+        } catch (MyArraySizeException e) {
+            System.out.println("Массив должен быть 4х4");
+        }
+    }
+    public static void MyArray (String[][] str){
+        System.out.println(Arrays.deepToString(str));
+    }
 
 }

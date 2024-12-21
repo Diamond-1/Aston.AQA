35a7eca4e5647b9c9c9355daf2dbfd0ee30f3901
Dima Tim <qa1717718@gmail.com>
2024-12-21 17:41:42 +0300

    Лекция 13.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,20 +5,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        //  Задание 1
-        String[][] str = new String[4][3];
-        try{
 
-            MyArray(str);
-        } catch (MyArraySizeException e) {
-            System.out.println("Вывод: " + e.getMessage());
-        }
-    }
-    public static void MyArray (String[][] str) throws MyArraySizeException {
-        if (str.length != 4 && str[0].length != 4){
-            throw new MyArraySizeException("Массив должен быть 4х4");
-        }
-        System.out.println(Arrays.deepToString(str));
     }
 
 }
